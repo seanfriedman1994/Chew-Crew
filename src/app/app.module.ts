0563e59be85274c6d6b482c9c14608319bfcc4a2
Sean Friedman <seanz1994@gmail.com>
2019-02-28 22:17:35 -0500

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { DishesComponent } from './dishes/dishes.component';
+import { GroupsComponent } from './groups/groups.component';
+import { RequestDishComponent } from './request-dish/request-dish.component';
+import { DishDetailComponent } from './dishes/dish-detail/dish-detail.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DishesComponent,
+    GroupsComponent,
+    RequestDishComponent,
+    DishDetailComponent,
+    SignUpComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/request-dish/request-dish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-dish',
+  templateUrl: './request-dish.component.html',
+  styleUrls: ['./request-dish.component.css']
+})
+export class RequestDishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
