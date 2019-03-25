cb40aa142129b4caef4a5fa0a1d68c84223cd823
derekmccr <42626160+derekmccr@users.noreply.github.com>
2019-03-25 18:22:53 -0400

    event api

---
+++ backend/routes/events.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const Dish = require('../models/events');
+
+const router = express.Router();
+
+router.post("", (req, res, next) => {
+    const event = new Event({
+        name: req.body.name,
+        date: req.body.date,
+        location: req.body.date,
+        description: req.body.description
+    });
+    dish.save().then(createdEvent => {
+        res.status(201).json({
+             message: "Event added successfully",
+             eventId: createdEvent._id
+        });
+    });
+});
+
+router.put("/:id", (req, res, next) => {
+    const event = new Event({
+      name: req.body.name,
+      date: req.body.date,
+      location: req.body.date,
+      description: req.body.description
+    });
+    Dish.updateOne({_id: req.params.id}, event).then(result => {
+        console.log(result);
+        res.status(200).json({message: "Update successful!"});
+    });
+});
+
+
+router.get("", (req, res, next) => {
+    Event.find().then(documents => {
+        res.status(200).json({
+            message: "Event fetched successfully!",
+            events: documents
+        });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+    Event.findById(req.params.id).then(event => {
+        if(event)
+        {
+            res.status(200).json(event);
+        }else
+        {
+            res.status(404).json({message: 'Event not found!'});
+        }
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+    Event.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({message: "Event deleted!"});
+    });
+});
+
+module.exports = router;
