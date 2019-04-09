d84cb1f8dc7bf509d3613dc0cfc26dcc802b05e1
Sean Friedman <seanz1994@gmail.com>
2019-04-09 19:16:36 -0400

    Production created

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb+srv://Sean:" + process.env.MONGO_ATLAS_PW + "@cluster0
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
